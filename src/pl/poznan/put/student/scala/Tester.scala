9532b2e3ab6b25cd7d758f12606715687b677d0a
phisikus <phisikus@platinum.linux.pl>
2015-01-12 23:53:01 +0100

    TreeDifference apply fixed

--- src/pl/poznan/put/student/scala/Tester.scala
+++ src/pl/poznan/put/student/scala/Tester.scala
@@ -1,6 +1,7 @@
 package pl.poznan.put.student.scala
 
 import java.io.{FileInputStream, FileOutputStream, ObjectInputStream, ObjectOutputStream}
+import java.util.Scanner
 
 import pl.poznan.put.student.scala.fsync.tree.DirectoryTree
 import pl.poznan.put.student.scala.fsync.tree.builder.TreeBuilder
@@ -31,19 +32,49 @@ object Tester extends App {
     tree
   }
 
-  val treeBuilder: TreeBuilder = Container.getTreeBuilder
-  val differenceGenerator = Container.getDifferenceGenerator
+  def testDetectChange() = {
+    val treeBuilder: TreeBuilder = Container.getTreeBuilder
+    val differenceGenerator = Container.getDifferenceGenerator
 
-  val tree = treeBuilder.generateTree("/home/phisikus/eagle")
-  val tree2 = loadTree()
+    val tree = treeBuilder.generateTree("/home/phisikus/eagle")
+    val tree2 = loadTree()
 
-  val difference = differenceGenerator.generate(tree2, tree)
-  println(tree.toString)
-  println(tree2.toString)
-  println(tree.equals(tree2))
-  println(difference.toString)
-  save(difference)
+    val difference = differenceGenerator.generate(tree2, tree)
+    println(tree.toString)
+    println(tree2.toString)
+    println(tree.equals(tree2))
+    println(difference.toString)
+    save(difference)
 
-  saveTree(tree)
+    saveTree(tree)
+
+  }
+
+  def testCreateTheSameInEmptyDir() = {
+    val input = new Scanner( System.in )
+
+    val treeBuilder: TreeBuilder = Container.getTreeBuilder
+    val differenceGenerator = Container.getDifferenceGenerator
+
+    val tree = treeBuilder.generateTree("/home/phisikus/eagle")
+    System.out.print("Change something, Press key...\n");
+    input.next()
+    val tree2 = treeBuilder.generateTree("/home/phisikus/eagle")
+    System.out.print("Revert something, Press key...\n");
+    input.next()
+
+
+    val difference = differenceGenerator.generate(tree2, tree)
+    println(tree.toString)
+    println(tree2.toString)
+    println(tree.equals(tree2))
+    println(difference.toString)
+    System.out.print("Now apply, Press key...\n");
+    input.next()
+
+    difference.apply()
+  }
+
+  testCreateTheSameInEmptyDir()
 
 }
