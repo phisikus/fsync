4ae8cabc9e10912284e73b3470ed719e37c288fd
Michal ElFartas <m.elfartas@gmail.com>
2015-01-13 19:51:36 +0100

    ServerCommunicator implemenetation & reorganization

--- src/main/scala/pl/poznan/put/student/scala/fsync/actors/Participant.scala
+++ src/main/scala/pl/poznan/put/student/scala/fsync/actors/Participant.scala
@@ -1,12 +1,11 @@
 package pl.poznan.put.student.scala.fsync.actors
 
-import pl.poznan.put.student.scala.fsync.communication.Communicator
 import pl.poznan.put.student.scala.fsync.communication.message.Message
 
 trait Participant {
 
-  def onMessageReceived(msg: Message, communicator: Communicator)
+  def onMessageReceived(msg: Message) : Message
 
-  def onInitialize(args: Array[String], communicator: Communicator)
+  def onInitialize(args: Array[String]) : Message
 
 }
