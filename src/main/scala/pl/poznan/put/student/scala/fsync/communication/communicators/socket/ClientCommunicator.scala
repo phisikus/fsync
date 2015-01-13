ef043349ef35e75af21e3efc7d2ef43440beae58
phisikus <phisikus@platinum.linux.pl>
2015-01-13 23:12:39 +0100

    Client & Server Communicators not tested

---
+++ src/main/scala/pl/poznan/put/student/scala/fsync/communication/communicators/socket/ClientCommunicator.scala
@@ -0,0 +1,46 @@
+package pl.poznan.put.student.scala.fsync.communication.communicators.socket
+
+import java.io.{ObjectInputStream, ObjectOutputStream}
+import java.net.Socket
+
+import pl.poznan.put.student.scala.fsync.actors.Participant
+import pl.poznan.put.student.scala.fsync.communication.Communicator
+import pl.poznan.put.student.scala.fsync.communication.message.{Message, ParticipantHandle}
+
+class ClientCommunicator(actor: Participant, args: Array[String]) extends Communicator {
+  override val participant: Participant = _
+  override val localHandle: ParticipantHandle = _
+  var clientSocket: Socket = _
+
+
+  def initialize() = {
+    localHandle.hostName = args(0)
+    clientSocket = new Socket(localHandle.hostName, localHandle.port)
+    val messageToSend = participant.onInitialize(args)
+    if (messageToSend != null) {
+      messageToSend.sender = localHandle
+      val outputToClient = new ObjectOutputStream(clientSocket.getOutputStream)
+      outputToClient.writeObject(messageToSend)
+      outputToClient.close()
+      var continue = true
+      while (continue) {
+        val inputFromServer = new ObjectInputStream(clientSocket.getInputStream)
+        val messageFromServer = inputFromServer.readObject().asInstanceOf[Message]
+        inputFromServer.close()
+        val messageToSend = participant.onMessageReceived(messageFromServer)
+        if (messageToSend != null) {
+          messageToSend.sender = localHandle
+          val outputToClient = new ObjectOutputStream(clientSocket.getOutputStream)
+          outputToClient.writeObject(messageToSend)
+          outputToClient.close()
+        } else {
+          continue = false
+        }
+      }
+    }
+    clientSocket.close()
+
+  }
+
+  initialize()
+}
--- src/main/scala/pl/poznan/put/student/scala/fsync/communication/communicators/socket/ServerCommunicator.scala
+++ src/main/scala/pl/poznan/put/student/scala/fsync/communication/communicators/socket/ServerCommunicator.scala
@@ -25,6 +25,11 @@ class ServerCommunicator(actor: Participant, args: Array[String]) extends Commun
 
   }
 
+
+  def initialize(): Unit = {
+    this.run()
+  }
+
   def handleConnectionInNewThread(connectionSocket: Socket) {
     new Thread(new Runnable {
       override def run() = {
@@ -36,14 +41,20 @@ class ServerCommunicator(actor: Participant, args: Array[String]) extends Commun
   def onClientConnected(connectionSocket: Socket) {
     println(Console.YELLOW + "Accepted connection from " + connectionSocket.getRemoteSocketAddress.toString + Console.RESET)
     val messageFromClient = getMessageFromClient(connectionSocket)
+    messageFromClient.sender = new ClientHandle()
+    messageFromClient.sender.hostName = connectionSocket.getRemoteSocketAddress.toString
+    println(Console.BLUE + "Received " + messageFromClient.messageType.toString + " message from " + connectionSocket.getRemoteSocketAddress.toString + Console.RESET)
     val messageToClient = participant.onMessageReceived(messageFromClient)
-
     if (messageToClient != null) {
       val outputToClient = new ObjectOutputStream(connectionSocket.getOutputStream)
+      messageToClient.sender = messageFromClient.recipient
       outputToClient.writeObject(messageToClient)
       outputToClient.close()
+      println(Console.BLUE + "Sent " + messageToClient.messageType.toString + " to " + connectionSocket.getRemoteSocketAddress.toString + Console.RESET)
+
     }
     connectionSocket.close()
+    println(Console.CYAN + "Connection closed with " + connectionSocket.getRemoteSocketAddress.toString + Console.RESET)
   }
 
   def getMessageFromClient(connectionSocket: Socket): Message = {
