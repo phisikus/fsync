ca5cc7562e3d2fd114e04a56a75e60c0ad04248f
Michal ElFartas <m.elfartas@gmail.com>
2015-01-13 19:51:36 +0100

    ServerCommunicator implemenetation & reorganization

--- src/main/scala/pl/poznan/put/student/scala/fsync/actors/Client.scala
+++ src/main/scala/pl/poznan/put/student/scala/fsync/actors/Client.scala
@@ -1,14 +1,13 @@
 package pl.poznan.put.student.scala.fsync.actors
 
-import pl.poznan.put.student.scala.fsync.communication.Communicator
 import pl.poznan.put.student.scala.fsync.communication.message.Message
 
 class Client extends Participant {
-  override def onMessageReceived(msg: Message, communicator: Communicator): Unit = {
-
+  override def onMessageReceived(msg: Message): Message = {
+    null
   }
 
-  override def onInitialize(args: Array[String], communicator: Communicator): Unit = {
-
+  override def onInitialize(args: Array[String]): Message = {
+    null
   }
 }
