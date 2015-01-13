771c443b7bc48e76d9a8d62e65d9851710f8cb41
phisikus <phisikus@platinum.linux.pl>
2015-01-13 11:12:04 +0100

    test stub

--- src/test/scala/pl/poznan/put/student/scala/fsync/test/DirectoryGenerator.scala
+++ src/test/scala/pl/poznan/put/student/scala/fsync/test/DirectoryGenerator.scala
@@ -1,8 +1,47 @@
 package pl.poznan.put.student.scala.fsync.test
 
 
-class DirectoryGenerator {
+import java.io.{FileOutputStream, File}
 
-  
+import scala.util.Random
 
+class DirectoryGenerator(currentPath: String) {
+
+  val randomGenerator = new Random()
+  val fileSize = 100000
+
+  def generateRandomContent(size: Int): Array[Byte] = {
+    val result = new Array[Byte](size)
+    randomGenerator.nextBytes(result)
+    result
+  }
+
+  def generateRandomFiles(currentPath: String, numberOfFiles: Int): Unit = {
+    for (i <- 1 until numberOfFiles) {
+      val outputStream = new FileOutputStream(currentPath + "/testFile_" + i.toString, false)
+      outputStream.write(generateRandomContent(fileSize))
+      outputStream.close()
+    }
+
+  }
+
+  def generateExampleDirectory(directoryName: String): Unit = {
+    val fullDirectoryName = currentPath + "/" + directoryName
+    val parentDirectory = new File(fullDirectoryName)
+    parentDirectory.mkdirs()
+    generateRandomFiles(fullDirectoryName, 10)
+  }
+
+  def removeExampleDirectory(directoryName: String): Unit = {
+    def delete(f: File) {
+      if (f.isDirectory) {
+        for (c <- f.listFiles)
+          delete(c)
+        f.delete()
+      } else {
+        f.delete()
+      }
+    }
+    delete(new File(currentPath + "/" + directoryName))
+  }
 }
--- src/test/scala/pl/poznan/put/student/scala/fsync/test/TreeRestoreTest.scala
+++ src/test/scala/pl/poznan/put/student/scala/fsync/test/TreeRestoreTest.scala
@@ -1,7 +1,27 @@
 package pl.poznan.put.student.scala.fsync.test
 
-import org.scalatest._
+import java.io.File
 
-class TreeRestoreTest extends FlatSpec {
+import org.scalatest._
+import pl.poznan.put.student.scala.fsync.tree.builder.DirectoryTreeBuilder
+
+class TreeRestoreTest extends FlatSpec with BeforeAndAfter {
+  val basePath = new File(".").getAbsolutePath
+  val exampleDirectoryName = "testDirectory"
+  val directoryTreeBuilder = new DirectoryTreeBuilder()
+  val directoryGenerator = new DirectoryGenerator(basePath)
+
+  before {
+    directoryGenerator.generateExampleDirectory(exampleDirectoryName)
+  }
+
+  after {
+    directoryGenerator.removeExampleDirectory(exampleDirectoryName)
+  }
+
+  "Directory Tree" should "be created properly" in {
+    val directoryTree = directoryTreeBuilder.generateTree(basePath + "/" + exampleDirectoryName)
+  }
+
 
 }
