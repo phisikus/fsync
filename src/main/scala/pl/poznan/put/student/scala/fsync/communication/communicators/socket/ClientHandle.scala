cbd1324184f0ada2020ab0e70dc1ab3eedf3076b
Michal ElFartas <m.elfartas@gmail.com>
2015-01-16 00:00:31 +0100

    Server&Client Communicators almost complete, FP improvements

--- src/main/scala/pl/poznan/put/student/scala/fsync/communication/communicators/socket/ClientHandle.scala
+++ src/main/scala/pl/poznan/put/student/scala/fsync/communication/communicators/socket/ClientHandle.scala
@@ -2,7 +2,7 @@ package pl.poznan.put.student.scala.fsync.communication.communicators.socket
 
 import pl.poznan.put.student.scala.fsync.communication.message.ParticipantHandle
 
-class ClientHandle extends ParticipantHandle {
-
-
+class ClientHandle(address: String) extends ParticipantHandle {
+  override val hostName: String = address
+  override val id: String = null
 }
