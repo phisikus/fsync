f20456a1e552e3cb405f50d48793b4f53770360e
Marcin Biernacki <marcin.biernacki@student.put.poznan.pl>
2015-01-15 15:30:45 +0100

    changed server organisation

--- src/main/scala/pl/poznan/put/student/scala/fsync/communication/message/MessageType.scala
+++ src/main/scala/pl/poznan/put/student/scala/fsync/communication/message/MessageType.scala
@@ -2,5 +2,5 @@ package pl.poznan.put.student.scala.fsync.communication.message
 
 object MessageType extends Enumeration {
   type MessageType = Value
-  val Pull, Push = Value
+  val Pull, PullPush, Push, Response, Goodbye = Value
 }
