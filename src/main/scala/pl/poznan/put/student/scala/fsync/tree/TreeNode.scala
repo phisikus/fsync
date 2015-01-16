47e31bc658f30e6a739dd127f0f3694d3a7f36ea
phisikus <phisikus@platinum.linux.pl>
2015-01-16 17:59:46 +0100

    pull working, hashCode added.

--- src/main/scala/pl/poznan/put/student/scala/fsync/tree/TreeNode.scala
+++ src/main/scala/pl/poznan/put/student/scala/fsync/tree/TreeNode.scala
@@ -25,10 +25,22 @@ trait TreeNode extends Serializable {
     }
     val node = obj.asInstanceOf[TreeNode]
     node match {
-      case n: TreeNode => name.equals(n.name) && hash.equals(n.hash) && isDirectory.equals(n.isDirectory) && isParentEqualIfPresent(n)
+      case _: TreeNode => name.equals(node.name) && hash.equals(node.hash) && isDirectory.equals(node.isDirectory) && isParentEqualIfPresent(node)
       case _ => false
     }
 
+  }
+
+
+  override def hashCode(): Int = {
+    var hash : Int = 1
+    hash = hash * 31 + name.hashCode
+    hash = hash * 31 + hash.hashCode
+    hash = hash * 31 + isDirectory.hashCode
+    hash = hash * 31 + children.length.hashCode
+    if (parent != null)
+      hash = hash * 31 + parent.hashCode
+    hash
   }
 
   def getFullPath: String = {
