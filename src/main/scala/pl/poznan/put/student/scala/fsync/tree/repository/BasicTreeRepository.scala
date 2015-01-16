05275e1b031b67e7cc4729b6e05c00a737021d73
phisikus <phisikus@platinum.linux.pl>
2015-01-16 18:43:04 +0100

    push done?

--- src/main/scala/pl/poznan/put/student/scala/fsync/tree/repository/BasicTreeRepository.scala
+++ src/main/scala/pl/poznan/put/student/scala/fsync/tree/repository/BasicTreeRepository.scala
@@ -8,19 +8,24 @@ import pl.poznan.put.student.scala.fsync.utils.Container
 class BasicTreeRepository extends TreeRepository {
   var forest: List[DirectoryTree] = List()
 
-  private def generateEmptyTree(name: String): DirectoryTree = {
-    val file = new File(name)
-    if(!file.exists) {
-      file.mkdirs()
-    }
-    Container.getTreeBuilder.generateTree(name)
-  }
-
   override def getDirectoryTree(directoryName: String): DirectoryTree = {
     val trees = forest.filter(x => x.path.equals(directoryName))
     trees.length match {
       case x if x > 0 => trees(0)
       case _ => generateEmptyTree(directoryName)
     }
+  }
+
+  private def generateEmptyTree(name: String): DirectoryTree = {
+    val file = new File(name)
+    if (!file.exists) {
+      file.mkdirs()
+    }
+    Container.getTreeBuilder.generateTree(name)
+  }
+
+  override def rebuildDirectoryTree(directoryName: String): Unit = {
+    forest = forest.filter(x => !x.path.equals(directoryName))
+    forest = forest :+ Container.getTreeBuilder.generateTree(directoryName)
   }
 }
--- src/main/scala/pl/poznan/put/student/scala/fsync/tree/repository/TreeRepository.scala
+++ src/main/scala/pl/poznan/put/student/scala/fsync/tree/repository/TreeRepository.scala
@@ -4,4 +4,5 @@ import pl.poznan.put.student.scala.fsync.tree.DirectoryTree
 
 trait TreeRepository {
   def getDirectoryTree(directoryName : String) : DirectoryTree
+  def rebuildDirectoryTree(directoryName : String)
 }
