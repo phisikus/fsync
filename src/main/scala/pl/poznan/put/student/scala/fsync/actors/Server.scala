4b43d889f9bc9356a3a8e961a7f2237320bd8bdd
phisikus <phisikus@platinum.linux.pl>
2015-01-16 17:59:46 +0100

    pull working, hashCode added.

--- src/main/scala/pl/poznan/put/student/scala/fsync/actors/Server.scala
+++ src/main/scala/pl/poznan/put/student/scala/fsync/actors/Server.scala
@@ -3,16 +3,21 @@ package pl.poznan.put.student.scala.fsync.actors
 import pl.poznan.put.student.scala.fsync.communication.message.{MessageType, Message}
 import pl.poznan.put.student.scala.fsync.utils.Container
 
+import scala.concurrent.Lock
+
 class Server extends Participant {
 
   val treeRepository = Container.getTreeRepository
   val differenceGenerator = Container.getDifferenceGenerator
+  val repositoryLock = new Lock()
 
   override def onMessageReceived(msg: Message): Message = {
     msg.messageType match {
       case MessageType.Pull =>
+        repositoryLock.acquire()
         val currentTree = treeRepository.getDirectoryTree(msg.tree.path)
         val difference = differenceGenerator.generate(msg.tree, currentTree)
+        repositoryLock.release()
         println(Console.BLUE + "Difference information about \"" + msg.tree.path + "\" created. There are " + difference.nodeDifferences.length.toString + " differences.")
         new Message(MessageType.PullResponse, null, difference)
       case _ => null
