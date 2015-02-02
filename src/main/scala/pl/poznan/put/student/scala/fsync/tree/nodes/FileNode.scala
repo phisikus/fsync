20773304420247b8215f17e7a417ce64c08f0694
phisikus <phisikus@platinum.linux.pl>
2015-02-02 22:36:06 +0100

    DirectoryTreeBuilder refactor

--- src/main/scala/pl/poznan/put/student/scala/fsync/tree/nodes/FileNode.scala
+++ src/main/scala/pl/poznan/put/student/scala/fsync/tree/nodes/FileNode.scala
@@ -4,18 +4,16 @@ import pl.poznan.put.student.scala.fsync.tree.TreeNode
 
 
 class FileNode(parentNode: TreeNode, fileName: String, fileChecksum: String) extends TreeNode {
-
-  override def parent: TreeNode = parentNode
-
   override var children: List[TreeNode] = List()
-
-  override def hash: String = fileChecksum
-
-  override def name: String = fileName
+  override var parent: TreeNode = parentNode
 
   override def toString: String = {
     "\n{ name: " + name + ", isDirectory: " + isDirectory + ", hash: " + hash + " }"
   }
+
+  override def hash: String = fileChecksum
+
+  override def name: String = fileName
 
   override def isDirectory: Boolean = false
 }
