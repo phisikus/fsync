c67d890d99e4f75dde1c31a60fcf000eb239aedc
phisikus <phisikus@platinum.linux.pl>
2015-02-03 11:32:28 +0100

    onCrash - release locks.

--- src/main/scala/pl/poznan/put/student/scala/fsync/utils/PathLock.scala
+++ src/main/scala/pl/poznan/put/student/scala/fsync/utils/PathLock.scala
@@ -8,34 +8,55 @@ class PathLock {
   val syncLock = new Lock
   val locks = new mutable.HashMap[String, Lock]()
 
-  def acquire(path: String): Unit = {
+  def acquire(path: String, listOfLocks: List[Lock]): List[Lock] = {
+    List(acquire(path)) ::: listOfLocks
+  }
+
+  def acquire(path: String): Lock = {
     syncLock.acquire()
     if (locks.contains(path)) {
       val lockToAcquire: Lock = locks.get(path).get
       syncLock.release()
       lockToAcquire.acquire()
+      lockToAcquire
     } else {
       val lockToAcquire = new Lock()
       locks += (path -> lockToAcquire)
       syncLock.release()
       lockToAcquire.acquire()
+      lockToAcquire
     }
 
   }
 
-  def release(path: String): Unit = {
+  def release(path: String, listOfLocks: List[Lock]): List[Lock] = {
+    val lock = release(path)
+    listOfLocks.filterNot(p => p.equals(lock))
+  }
+
+  def release(path: String): Lock = {
     syncLock.acquire()
     if (locks.contains(path)) {
       val lockToRelease: Lock = locks.get(path).get
       syncLock.release()
       lockToRelease.release()
+      lockToRelease
     } else {
       val lockToRelease = new Lock()
       locks += (path -> lockToRelease)
       syncLock.release()
       lockToRelease.release()
+      lockToRelease
     }
 
+  }
+
+  def releaseListOfLocks(list: List[Lock]): Unit = {
+    list match {
+      case head :: tail => head.release()
+        releaseListOfLocks(tail)
+      case Nil => ()
+    }
   }
 
 }
